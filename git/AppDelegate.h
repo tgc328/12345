8018219307b74ef4941e01279da79c37690a3f54
多乐米 <duolemi@duolemidemacbook-pro.local>
2016-02-06 17:14:40 +0800

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by 多乐米 on 16/2/6.
+//  Copyright © 2016年 duolemi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  git
+//
+//  Created by 多乐米 on 16/2/6.
+//  Copyright © 2016年 duolemi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
